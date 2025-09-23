2435f2c42ec17772862b64cbed4ed86ed906863b
a.vovnenko <vovnenkoandrii@gmail.com>
2025-09-23 23:12:57 +0200

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  // 
+  globalIgnores(['dist']), 
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -25,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'semi': ['error', 'always'],
+      'indent': ['error', 2],
     },
   },
 ])
